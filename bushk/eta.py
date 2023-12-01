a2bf51d6ddce512dba596f76e2d8769467745931
Maxx™️ <max.gamil110331@gmail.com>
2023-12-01 23:03:53 +0800

    0.0.1b

--- bushk/eta.py
+++ bushk/eta.py
@@ -18,4 +18,4 @@ class ETA:
         _data=json.loads(requests.get(f"https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/{stop.id}").content.decode("utf-8"))
         _list=[]
         for _route_data in _data["data"]:
-            _list.append({"route": _route_data["route"], "bound": _route_data["bound"], "service_type": _route_data["service_type"], "arrive_at": _data["eta"].replace("T", " ")[:-6], "rmk": {"tc": _data["rmk_tc"], "sc": _data["rmk_sc"], "en": _data["rmk_en"]}})
+            _list.append({"route": _route_data["route"], "bound": _route_data["bound"], "service_type": _route_data["service_type"], "arrive_at": _data["eta"].replace("T", " ")[:-6], "seq": _data["seq"], "rmk": {"tc": _data["rmk_tc"], "sc": _data["rmk_sc"], "en": _data["rmk_en"]}})
